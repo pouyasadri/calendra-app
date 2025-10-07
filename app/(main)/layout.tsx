898e1746183e4bfd2ab436830a5cf22d19673635
pouyasadri <pouyapaghandeh78@gmail.com>
2025-10-07 17:09:07 +0200

    Add navigation components for authenticated and public users

---
+++ app/(main)/layout.tsx
@@ -0,0 +1,18 @@
+import {currentUser} from "@clerk/nextjs/server";
+import React from "react";
+import PublicNavbar from "@/components/PublicNavbar";
+import PrivateNavbar from "@/components/PrivateNavbar";
+
+export default async function MainLayout({children}: { children: React.ReactNode }) {
+    const user = await currentUser();
+    return (
+        <main className="relative">
+
+            {user ? <PrivateNavbar/> : <PublicNavbar/>}
+
+            <section className="pt-36">
+                {children}
+            </section>
+        </main>
+    )
+}
